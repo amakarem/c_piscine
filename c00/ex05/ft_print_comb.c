ea3f7f4928ee859f034724da49ca9e5ac96c41dd
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-11 23:39:18 +0200

    ex05 norminette check

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 17:36:33 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/11 23:13:25 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/04/11 23:38:57 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,23 @@ void	ft_output(int a, int b, int c)
 	ft_putchar(a);
 	ft_putchar(b);
 	ft_putchar(c);
-	if(a == '7' && b == '8' && c == '9') {
-		return;
+	if (a == '7' && b == '8' && c == '9')
+	{
+		return ;
 	}
 	ft_putchar(',');
 	ft_putchar(' ');
 }
 
-
-
 void	ft_print_comb(void)
 {
-	int a;
-	int b;
-	int c;
+	int	a;
+	int	b;
+	int	c;
 
 	a = '0';
 	b = '1';
 	c = '2';
-
 	while (a <= '7')
 	{
 		while (b <= '8')

0990ea0aea2710c2dfc83b2f1cf293ebf4ec8f1a
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-13 14:06:55 +0200

    sync updates

--- rush00/rush01.c
+++ rush00/rush01.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 22:30:52 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/13 14:00:46 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/04/13 14:06:04 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@ void	ft_putchar(char c);
 
 void	rush(int x, int y)
 {
-    int	v;
-    int	h;
+	int	v;
+	int	h;
 
-    v=1;
+	v=1;
     while (v <= y)
     {
         h=1;
@@ -26,29 +26,29 @@ void	rush(int x, int y)
             if(h == 1) {
                 if(v == 1) {
                     ft_putchar('/');
-                } else if (v == y) {
-                    ft_putchar('\\');
-                }else {
-                    ft_putchar('*');
-                }
-            } else if(h == x) {
-                if(v == 1) {
-                    ft_putchar('\\');
-                } else if (v == y) {
-                    ft_putchar('/');
-                }else {
-                    ft_putchar('*');
-                }
-            } else {
-                if(v != y && v != 1) {
-                    ft_putchar(' ');
-                } else {
-                    ft_putchar('*');
-                }
-            }
-            h++;
-        }
-        ft_putchar('\n');
-        v++;
-    }
+				} else if (v == y) {
+					ft_putchar('\\');
+				}else {
+					ft_putchar('*');
+				}
+				} else if(h == x) {
+				if(v == 1) {
+					ft_putchar('\\');
+				} else if (v == y) {
+					ft_putchar('/');
+				}else {
+					ft_putchar('*');
+				}
+				} else {
+				if (v != y && v != 1) {
+					ft_putchar(' ');
+				} else {
+					ft_putchar('*');
+				}
+			}
+			h++;
+		}
+		ft_putchar('\n');
+		v++;
+	}
 }

88d73469246590f3c0398a74f315273a2a07e7b7
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-14 00:04:57 +0200

    ex01:

---
+++ c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 23:42:17 by aelaaser          #+#    #+#             */
+/*   Updated: 2024/04/14 00:03:20 by aelaaser         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/* un comment this part to test
+#include <stdio.h>
+int	main(void)
+{
+	int n;
+	int *pointer_1;
+	int **pointer_2;
+	int ***pointer_3;
+	int ****pointer_4;
+	int *****pointer_5;
+	int ******pointer_6;
+	int *******pointer_7;
+	int ********pointer_8;
+	int *********pointer_9;
+
+	n = 5;
+	pointer_1 = &n;
+	pointer_2 = &pointer_1;
+	pointer_3 = &pointer_2;
+	pointer_4 = &pointer_3;
+	pointer_5 = &pointer_4;
+	pointer_6 = &pointer_5;
+	pointer_7 = &pointer_6;
+	pointer_8 = &pointer_7;
+	pointer_9 = &pointer_8;
+	ft_ft(pointer_9);
+	printf("%d", n);
+
+}
+*/

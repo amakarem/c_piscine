18fd6e3a9df49987ceb1022c6bc9bd2827273d6d
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-13 14:54:56 +0200

    split rush function to 4 functions

--- rush00/rush01.c
+++ rush00/rush01.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   rush01.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/12 22:30:52 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/13 14:06:04 by aelaaser         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   rush01.c										   :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: aelaaser <aelaaser@student.42.fr>		  +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2024/04/12 22:30:52 by aelaaser		  #+#	#+#			 */
+/*   Updated: 2024/04/13 14:30:09 by aelaaser		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
@@ -17,38 +17,71 @@ void	rush(int x, int y)
 	int	v;
 	int	h;
 
-	v=1;
-    while (v <= y)
-    {
-        h=1;
-        while (h <= x)
-        {
-            if(h == 1) {
-                if(v == 1) {
-                    ft_putchar('/');
-				} else if (v == y) {
-					ft_putchar('\\');
-				}else {
-					ft_putchar('*');
-				}
-				} else if(h == x) {
-				if(v == 1) {
-					ft_putchar('\\');
-				} else if (v == y) {
-					ft_putchar('/');
-				}else {
-					ft_putchar('*');
-				}
-				} else {
-				if (v != y && v != 1) {
-					ft_putchar(' ');
-				} else {
-					ft_putchar('*');
-				}
+	v = 1;
+	while (v <= y)
+	{
+		h = 1;
+		while (h <= x)
+		{
+			if (h == 1)
+			{
+				first_char(x, y, h, v);
+			}
+			else if (h == x)
+			{
+				last_char(x, y, h, v);
+			}
+			else
+			{
+				mid_char(x, y, h, v);
 			}
 			h++;
 		}
-		ft_putchar('\n');
 		v++;
 	}
+}
+
+void	first_char(int x, int y, int h, int v)
+{
+	if (v == 1)
+	{
+		ft_putchar('/');
+	}
+	else if (v == y)
+	{
+		ft_putchar('\\');
+	}
+	else
+	{
+		ft_putchar('*');
+	}
+}
+
+void	mid_char(int x, int y, int h, int v)
+{
+	if (v != y && v != 1)
+	{
+		ft_putchar(' ');
+	}
+	else
+	{
+		ft_putchar('*');
+	}
+}
+
+void	last_char(int x, int y, int h, int v)
+{
+	if (v == 1)
+	{
+		ft_putchar('\\');
+	}
+	else if (v == y)
+	{
+		ft_putchar('/');
+	}
+	else
+	{
+		ft_putchar('*');
+	}
+	ft_putchar('\n');
 }

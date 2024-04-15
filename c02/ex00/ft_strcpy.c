0f62d69f7c39c1180e1b4347169abb5718054228
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-15 22:35:16 +0200

    ex00

---
+++ c02/ex00/ft_strcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 22:06:18 by aelaaser          #+#    #+#             */
+/*   Updated: 2024/04/15 22:34:59 by aelaaser         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (*dest);
+}
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char	dest[] = "xxxxxxxxxxx";
+// 	char	src[] = "Hello World";
+
+// 	printf("%s\n", dest);
+// 	ft_strcpy(dest, src);
+// 	printf("%s\n", dest);
+// }

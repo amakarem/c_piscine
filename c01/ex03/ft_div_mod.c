934ca1aec59c1699ae4901800d44598828ebc852
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-15 15:18:12 +0200

    ex03

--- c01/ex03/ft_div_mod.c
+++ c01/ex03/ft_div_mod.c
@@ -1,40 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/14 00:06:52 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/15 15:17:45 by aelaaser         ###   ########.fr       */
+/*   Created: 2024/04/15 15:04:39 by aelaaser          #+#    #+#             */
+/*   Updated: 2024/04/15 15:17:15 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_swap(int *a, int *b)
+void	ft_div_mod(int a, int b, int *div, int *mod)
 {
-	int	x;
-
-	x = *a;
-	*a = *b;
-	*b = x;
+	*div = (a / b);
+	*mod = (a % b);
 }
 
-/* un comment this part to test *
+/* un comment this part to test 
 #include <stdio.h>
 int	main(void)
 {
 	int a;
 	int b;
-	int *pointer_a;
-	int *pointer_b;
+	int x;
+	int y;
 
-	a = 5;
-	b = 9;
-	pointer_a = &a;
-	pointer_b = &b;
-	ft_swap(pointer_a, pointer_b);
-	printf("%d", a);
-	printf("%d", b);
+	a = 9;
+	b = 4;
+
+	ft_div_mod(a , b, &x, &y);
+	printf("%d", x);
+	printf("%c", '\n');
+	printf("%d", y);
 
 }
 */

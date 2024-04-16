39f69c39ae7e1c85143f890ef96b407d0580f68d
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-16 20:07:22 +0200

    ex10

---
+++ c02/ex10/ft_strlcpy.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 19:50:32 by aelaaser          #+#    #+#             */
+/*   Updated: 2024/04/16 20:07:06 by aelaaser         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(dest));
+}
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char	src[] = "Hello World";
+// 	char	dest[] = "xxxxx";
+
+// 	printf("%d\n \n", ft_strlcpy(dest, src, 11));
+// 	printf("%d\n", ft_strlcpy(dest, src, 5));
+// }

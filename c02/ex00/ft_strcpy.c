a6225be42b1a17a900ef569f14f50e93ab64024a
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-16 23:35:42 +0200

    fix ex00 and ex01

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 22:06:18 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/16 23:29:28 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/04/16 23:35:20 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ char	*ft_strcpy(char *dest, char *src)
 	int	i;
 
 	i = 0;
-	while (src[i] != != '\0')
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
@@ -27,7 +27,7 @@ char	*ft_strcpy(char *dest, char *src)
 // #include <stdio.h>
 // int	main(void)
 // {
-// 	char	dest[] = "xxxxxxxxxxx";
+// 	char	dest[] = "xxxxx";
 // 	char	src[] = "Hello World";
 
 // 	printf("%s\n", dest);
--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 22:36:25 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/16 23:32:56 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/04/16 23:34:30 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,14 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	return (dest);
 }
 
-#include <stdio.h>
-int	main(void)
-{
-	char	src[] = "Hello World";
-	char	dest[] = "xxxxx";
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char	src[] = "Hello World";
+// 	char	dest[] = "xxxxx";
 
-	printf("%s\n \n", src);
-	printf("%s\n \n", dest);
-	ft_strncpy(dest, src, 5);
-	printf("%s\n", dest);
-}
+// 	printf("%s\n \n", src);
+// 	printf("%s\n \n", dest);
+// 	ft_strncpy(dest, src, 5);
+// 	printf("%s\n", dest);
+// }

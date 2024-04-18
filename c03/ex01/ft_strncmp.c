f71c91e97e64afcb7908bb58ed9a2ffe171e9cf2
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-18 20:34:37 +0200

    fix ex01

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:14:00 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/17 18:18:14 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/04/18 20:34:24 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,20 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && i < (n - 1))
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < (n - 1))
 	{
 		i++;
 	}
-	if (s1[i] == s2[i])
-		return (0);
-	else
-		return (s1[i] - s2[i]);
+	return (s1[i] - s2[i]);
 }
 
 // #include <stdio.h>
+// #include <string.h>
 // int main(void)
 // {
-// 	char* s1 = "abcdeFg";
-// 	char* s2 = "abcdefg";
+// 	char* s1 = "abaGXadss";
+// 	char* s2 = "abaGaadss";
 
+// 	printf("%d\n", strncmp(s1, s2, 5));
 // 	printf("%d\n", ft_strncmp(s1, s2, 5));
-// 	printf("%d\n", ft_strncmp(s1, s2, 7));
 // }

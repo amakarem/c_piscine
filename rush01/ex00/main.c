b2eff01d03ac1003b2c7bd476116c5032a8f3c92
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-21 01:01:55 +0200

    new solving function

--- rush01/ex00/main.c
+++ rush01/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 16:28:20 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/21 00:35:36 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/04/21 01:01:02 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,6 +101,64 @@ void	ft_solve_col_next(void)
 	}
 }
 
+void	ft_col_find_4(void)
+{
+	int	i;
+	int x;
+
+	i = 0;
+	while(i < 4)
+	{
+		if (g_matrix[0][i] <= g_matrix[1][i] && g_output[0][i] == 0)
+		{
+			g_output[0][i] = 4 - g_matrix[0][i] + 1;
+			x = 1;
+			while (g_output[x][i] == 0 && x < 4 && g_output[x + 1][i] <= 3)
+			{
+				g_output[x][i] = g_output[x + 1][i] + 1;
+				x++;
+			}
+
+		}
+		if (g_matrix[0][i] >= g_matrix[1][i] && g_output[0][i] == 0)
+		{
+			g_output[3][i] = 4 - g_matrix[1][i] + 1;
+			x = 2;
+			while (g_output[x][i] == 0 && x >= 0 && g_output[x + 1][i] <= 3)
+			{
+				g_output[x][i] = g_output[x + 1][i] + 1;
+				x--;
+			}
+
+		}
+		i++;
+	}
+}
+void	ft_solve_row_next(void)
+{
+	int	col;
+	int	row;
+
+	row = 0;
+	while (row < 4)
+	{
+		col = 0;
+		while (col < 4)
+		{
+			if (g_output[row][col] != 0 && g_output[row][col] <= 4)
+			{
+				g_output[row + 1][col] = g_output[row][col] + 1;
+				if (g_output[row + 1][col] == 5)
+				{
+					g_output[row + 1][col] = 1;
+				}
+			}
+			col++;
+		}
+		row++;
+	}
+}
+
 void	ft_solve_row_4(void)
 {
 	int	row;
@@ -145,6 +203,8 @@ int	main(int argc, char	**argv)
 	ft_solve_col_4();
 	ft_solve_row_4();
 	ft_solve_col_next();
+	ft_solve_row_next();
+	ft_col_find_4();
 	ft_print_output(g_output);
 	return (0);
 }

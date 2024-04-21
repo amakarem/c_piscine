54ab8bf2c4a3eb672687ea2862a3ec99c9ec7bab
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-21 04:17:08 +0200

    improvment

--- rush01/ex00/ft_col_find_4_ext.c
+++ rush01/ex00/ft_col_find_4_ext.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 03:19:00 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/21 03:20:19 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/04/21 04:05:07 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	ft_col_find_4_ext1(int i, int g_out[4][4])
 	x = 1;
 	while (g_out[x][i] == 0 && x < 4 && g_out[x + 1][i] <= 3)
 	{
-		g_out[x][i] = g_out[x + 1][i] + 1;
+		if (g_out[x + 1][i] != 0)
+			g_out[x][i] = g_out[x + 1][i] + 1;
 		x++;
 	}
 	return (0);
@@ -30,7 +31,8 @@ int	ft_col_find_4_ext2(int i, int g_out[4][4])
 	x = 2;
 	while (g_out[x][i] == 0 && x >= 0 && g_out[x + 1][i] <= 3)
 	{
-		g_out[x][i] = g_out[x + 1][i] + 1;
+		if (g_out[x + 1][i] != 0)
+			g_out[x][i] = g_out[x + 1][i] + 1;
 		x--;
 	}
 	return (0);

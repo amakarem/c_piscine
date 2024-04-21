1927331d7af5974d8fe01962f87f34c17fc8969e
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-21 06:54:38 +0200

    fix ex05

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:38:44 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/21 06:26:19 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/04/21 06:54:18 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_strlen(char *str)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		i++;
 	}
@@ -27,9 +27,8 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	unsigned int	dest_size;
 	unsigned int	src_size;
 	unsigned int	total_size;
+	unsigned int	i;
 
-	if ((!dest || !src) && !size)
-		return (0);
 	dest_size = ft_strlen(dest);
 	src_size = ft_strlen(src);
 	total_size = src_size + size;
@@ -41,17 +40,28 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	{
 		total_size = (dest_size + src_size);
 	}
+	i = dest_size;
+	while (src[i] && i < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i != dest_size)
+		dest[dest_size] = '\0';
 	return (total_size);
 }
 
-#include <stdio.h>
-#include <string.h>
-int main(void)
-{
-	char dest[] = "sss";
-	char src[] = "eeee";
-	printf("%d\n", ft_strlcat(dest, src, 0));
-	printf("%lu\n", strlcat(dest, src, 0));
-	printf("%s\n", dest);
-	printf("%s\n", src);
-}
+// #include <stdio.h>
+// #include <string.h>
+// int main(void)
+// {
+// 	unsigned int testval = 6;
+// 	char dest[] = "sss555";
+// 	char src[] = "eeee";
+
+	
+// 	printf("%d\n", ft_strlcat(dest, src, testval));
+// 	printf("%lu\n", strlcat(dest, src, testval));
+// 	printf("%s\n", dest);
+// 	printf("%s\n", src);
+// }

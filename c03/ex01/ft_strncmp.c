14d48c77fc993b2946dced5e2ff4673595227eb2
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-21 06:14:12 +0200

    fix strncmp

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:14:00 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/18 20:34:24 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/04/21 06:14:01 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
+	if (n == 0) {
+		return (0);
+	}
 	i = 0;
 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < (n - 1))
 	{
@@ -26,9 +29,9 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 // #include <string.h>
 // int main(void)
 // {
-// 	char* s1 = "abaGXadss";
-// 	char* s2 = "abaGaadss";
+// 	char s1[] = "@@-1,7@@";
+// 	char s2[] = "@@+1.7@@";
 
-// 	printf("%d\n", strncmp(s1, s2, 5));
-// 	printf("%d\n", ft_strncmp(s1, s2, 5));
+// 	printf("%d\n", strncmp(s1, s2, 0));
+// 	printf("%d\n", ft_strncmp(s1, s2, 0));
 // }

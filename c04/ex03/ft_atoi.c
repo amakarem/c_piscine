19670eed2a1c02f8cf0fd62114ab0356c027b01a
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-22 21:41:43 +0200

    ex03

---
+++ c04/ex03/ft_atoi.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 21:07:56 by aelaaser          #+#    #+#             */
+/*   Updated: 2024/04/22 21:41:27 by aelaaser         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	negative;
+	int	nb;
+
+	negative = 0;
+	nb = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '-')
+			negative++;
+		else if (str[i] >= '0' && str[i] <= '9')
+		{
+			if (nb != 0)
+				nb = nb * 10;
+			nb = nb + (str[i] - 48);
+		}
+		else if (nb != 0 && (str[i] > '9' || str[i] < '0'))
+			break ;
+		i++;
+	}
+	if (negative % 2 != 0)
+		nb = nb * -1;
+	return (nb);
+}
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	printf("%i\n", ft_atoi("567"));
+// 	printf("%i\n", ft_atoi(" ---+--+1234ab567"));
+// 	printf("%i\n", ft_atoi(" s234ab567"));
+// 	return (0);
+// }

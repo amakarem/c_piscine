70e692d64e3c97232965c8b00e2379461a24cba9
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-23 15:56:43 +0200

    ex03

--- c05/ex03/ft_recursive_power.c
+++ c05/ex03/ft_recursive_power.c
@@ -1,32 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/23 15:40:02 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/23 15:56:21 by aelaaser         ###   ########.fr       */
+/*   Created: 2024/04/23 15:51:05 by aelaaser          #+#    #+#             */
+/*   Updated: 2024/04/23 15:56:14 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_power(int nb, int power)
+int	ft_recursive_power(int nb, int power)
 {
-	int	result;
-
-	result = 1;
 	if ((nb == 0 && power == 0) || power < 0)
 		return (0);
-	while (power > 0)
-	{
-		result = (result * nb);
-		power--;
-	}
-	return (result);
+	if (power == 0)
+		return (1);
+	return (nb * ft_recursive_power(nb, power - 1));
 }
 
 // #include <stdio.h>
 // int	main(void)
 // {
-// 	printf("%i\n", ft_iterative_power(6, 3));
+// 	printf("%i\n", ft_recursive_power(6, 2));
 // }

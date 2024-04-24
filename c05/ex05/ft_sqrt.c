353e909e882bec400b7634dd4858faf4e005cbda
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-04-24 16:21:39 +0200

    fix issues

--- c05/ex05/ft_sqrt.c
+++ c05/ex05/ft_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 16:22:29 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/23 18:12:14 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/04/24 16:12:56 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ int	ft_sqrt(int nb)
 
 	i = 1;
 	calc = 1;
-	if (nb == 0 || nb == 2)
+	if (nb == 0)
 		return (0);
 	if (nb == 1)
 		return (1);
 	while (calc <= nb)
 	{
 		i++;
+		if (i > 46340)
+			return (0);
 		calc = i * i;
 	}
 	i = i - 1;
@@ -39,5 +41,5 @@ int	ft_sqrt(int nb)
 // 	printf("%i\n", ft_sqrt(0));
 // 	printf("%i\n", ft_sqrt(2));
 // 	printf("%i\n", ft_sqrt(1));
-// 	printf("%i\n", ft_sqrt(200));
+// 	printf("%i\n", ft_sqrt(10000));
 // }
--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 18:14:39 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/23 18:22:28 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/04/24 16:19:33 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@ int	ft_is_prime(int nb)
 	int	i;
 
 	i = 2;
-	if (nb == 1)
+	if (nb < 2)
 		return (0);
-	while (i <= 9)
+	while (i <= (nb / 2))
 	{
 		if (nb != i && nb % i == 0)
 		{

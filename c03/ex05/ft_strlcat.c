553445f4351883ea332d73be7cd5fc59d12d971c
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-07-19 14:59:54 +0200

    fix norm

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:38:44 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/21 06:54:18 by aelaaser         ###   ########.fr       */
+/*   Updated: 2025/07/19 14:59:39 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,29 +26,20 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	dest_size;
 	unsigned int	src_size;
-	unsigned int	total_size;
 	unsigned int	i;
 
 	dest_size = ft_strlen(dest);
 	src_size = ft_strlen(src);
-	total_size = src_size + size;
-	if (size > dest_size)
+	if (size <= dest_size)
+		return (size + src_size);
+	i = 0;
+	while (src[i] && (dest_size + i) < size - 1)
 	{
-		total_size = total_size + src_size;
-	}
-	if (total_size > (dest_size + src_size))
-	{
-		total_size = (dest_size + src_size);
-	}
-	i = dest_size;
-	while (src[i] && i < size)
-	{
-		dest[i] = src[i];
+		dest[dest_size + i] = src[i];
 		i++;
 	}
-	if (i != dest_size)
-		dest[dest_size] = '\0';
-	return (total_size);
+	dest[dest_size + i] = '\0';
+	return (dest_size + src_size);
 }
 
 // #include <stdio.h>
@@ -57,9 +48,7 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 // {
 // 	unsigned int testval = 6;
 // 	char dest[] = "sss555";
-// 	char src[] = "eeee";
-
-	
+// 	char src[] = "eeee";	
 // 	printf("%d\n", ft_strlcat(dest, src, testval));
 // 	printf("%lu\n", strlcat(dest, src, testval));
 // 	printf("%s\n", dest);

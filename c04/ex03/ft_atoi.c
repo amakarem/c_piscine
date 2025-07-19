6b426da7789f374276c7a4f9b7d20ad9bb39d546
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-07-19 15:17:39 +0200

    fix norm

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,9 +6,18 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 21:07:56 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/04/23 00:32:28 by aelaaser         ###   ########.fr       */
+/*   Updated: 2025/07/19 15:17:28 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	isspaceortab(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n')
+		return (1);
+	else if (c == '\v' || c == '\f' || c == '\r')
+		return (1);
+	return (0);
+}
 
 int	ft_atoi(char *str)
 {
@@ -31,7 +40,7 @@ int	ft_atoi(char *str)
 		}
 		else if (nb != 0 && (str[i] > '9' || str[i] < '0'))
 			return (nb * negative);
-		else if (str[i] != ' ' && str[i] != '+')
+		else if (!isspaceortab(str[i]) && str[i] != '+')
 			if (str[i] > '9' || str[i] < '0')
 				return (0);
 		i++;
@@ -42,8 +51,11 @@ int	ft_atoi(char *str)
 // #include <stdio.h>
 // int main(void)
 // {
-// 	printf("%i\n", ft_atoi("567"));
-// 	printf("%i\n", ft_atoi(" ---+----+1234ab567"));
-// 	printf("%i\n", ft_atoi(" s234ab567"));
+// 	printf("%i\n", ft_atoi("\t\n\v\f\r      23"));
+// 	printf("%i\n", ft_atoi("     ------+++++---12341"));
+// 	printf("%i\n", ft_atoi("     ------+++++---123413"));
+// 	printf("%i\n", ft_atoi("00000000002147483647"));
+// 	printf("%i\n", ft_atoi("-2147483648"));
+// 	printf("%i\n", ft_atoi("a"));
 // 	return (0);
 // }
